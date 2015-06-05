4286b397cd4fded8f5dec27007b52077fb1dd220
Colin Newell <colin@opusvl.com>
2015-06-05 08:02:14 +0100

    Removed boiler plate

--- lib/Code4Health/LDAP.pm
+++ lib/Code4Health/LDAP.pm
@@ -1,12 +1,10 @@
 package Code4Health::LDAP;
 
-use 5.006;
-use strict;
-use warnings FATAL => 'all';
+use Moo;
 
 =head1 NAME
 
-Code4Health::LDAP - The great new Code4Health::LDAP!
+Code4Health::LDAP - Wrapper around LDAP
 
 =head1 VERSION
 
@@ -19,35 +17,15 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+This module will allow access to users and groups within LDAP.
 
     use Code4Health::LDAP;
 
-    my $foo = Code4Health::LDAP->new();
+    my $ldap = Code4Health::LDAP->new();
+    $ldap->add_user('test');
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head1 METHODS
 
 =head1 AUTHOR
 
@@ -58,38 +36,6 @@ OpusVL, C<< <support at opusvl.com> >>
 Please report any bugs or feature requests to C<bug-code4health-ldap at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Code4Health-LDAP>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Code4Health::LDAP
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code4Health-LDAP>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Code4Health-LDAP>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Code4Health-LDAP>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Code4Health-LDAP/>
-
-=back
 
 
 =head1 ACKNOWLEDGEMENTS
